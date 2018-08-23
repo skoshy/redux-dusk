9208e67bd08d0397dfd538663933d7a5d6c18b82
Stefan Koshy <noreply@skoshy.com>
2018-08-23 09:15:31 -0400

    Static Docs

---
+++ examples/web-static/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Router, Link } from 'react-static';
+import { hot } from 'react-hot-loader';
+import { Provider } from 'react-redux';
+
+import Routes from 'react-static-routes'; /* eslint-disable-line import/no-unresolved, import/extensions */
+
+import { store } from './store';
+import './app.css';
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div>
+        <nav>
+          <Link exact to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <div className="content">
+          <Routes />
+        </div>
+      </div>
+    </Router>
+  </Provider>
+);
+
+export default hot(module)(App);
