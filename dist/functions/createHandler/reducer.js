f0f05d08871cf2d7479c7a552ce4965611151ae4
Stefan Koshy <noreply@skoshy.com>
2018-08-31 11:49:56 -0400

    Added modified dist files

--- dist/functions/createHandler/reducer.js
+++ dist/functions/createHandler/reducer.js
@@ -7,26 +7,70 @@ exports.parseReducer = undefined;
 
 var _helpers = require('../../helpers');
 
+function parseReducerInnerLoop(parseParam) {
+  var toReturn = {};
+
+  if ((0, _helpers.isObject)(parseParam)) {
+    // this is an object, e.x. {loading: false, error: 'Nothing found'}
+    // let's loop through this object and set params accordingly
+    var parseParamKeys = Object.keys(parseParam);
+    parseParamKeys.forEach(function (parseParamKey) {
+      var parseInnerParam = parseParam[parseParamKey];
+      toReturn[parseParamKey] = parseInnerParam;
+    });
+  } else {
+    // it's a string, so just set it to undefined
+    toReturn[parseParam] = undefined;
+  }
+
+  return toReturn;
+}
+
 var parseReducer = exports.parseReducer = function parseReducer(currentType, fullType, reducerParams, initialState) {
-  if ((0, _helpers.isArray)(reducerParams)) {
+  var thingsToReduce = reducerParams;
+  var thingsToReset = [];
+  var clonedInitialState = (0, _helpers.cloneObj)(initialState);
+
+  if ((0, _helpers.isObject)(reducerParams)) {
+    thingsToReduce = [];
+    if (reducerParams.reduce) {
+      thingsToReduce = reducerParams.reduce;
+    }
+
+    if (reducerParams.reset) {
+      if (reducerParams.reset === true) {
+        // end here, everything should be reset
+        return function () {
+          var clonedInitialStateInner = (0, _helpers.cloneObj)(initialState); // we must clone it in here again
+          (0, _helpers.debugLog)({}, 'Resetting to initial state for type', fullType);
+          return Object.assign({}, clonedInitialStateInner);
+        };
+      }
+
+      thingsToReset = reducerParams.reset;
+    }
+  }
+
+  (0, _helpers.debugLog)({}, 'Type', fullType, 'Things to reduce', thingsToReduce, 'Things to reset', thingsToReset);
+
+  if ((0, _helpers.isArray)(thingsToReduce)) {
     // it's an array; we'll append these new values to the state, e.x. ['todos', 'lastUpdated']
     var addToState = {};
-    reducerParams.forEach(function (parseParam) {
-      if ((0, _helpers.isObject)(parseParam)) {
-        // this is an object, e.x. {loading: false, error: 'Nothing found'}
-        // let's loop through this object and set params accordingly
-        var parseParamKeys = Object.keys(parseParam);
-        parseParamKeys.forEach(function (parseParamKey) {
-          var parseInnerParam = parseParam[parseParamKey];
-          addToState[parseParamKey] = parseInnerParam;
-        });
-      } else {
-        // it's a string, so just set it to undefined
-        addToState[parseParam] = undefined;
+    var addToStateKeys = Object.keys(addToState);
+    var resetFromInitialState = {};
+
+    thingsToReduce.forEach(function (parseParam) {
+      addToState = Object.assign({}, addToState, parseReducerInnerLoop(parseParam));
+    });
+
+    thingsToReset.forEach(function (parseParam) {
+      if (clonedInitialState[parseParam]) {
+        resetFromInitialState[parseParam] = clonedInitialState[parseParam];
       }
     });
 
-    var addToStateKeys = Object.keys(addToState);
+    // update since we just added a bunch of keys
+    addToStateKeys = Object.keys(addToState);
 
     return function (state, action) {
       addToStateKeys.forEach(function (key) {
@@ -35,19 +79,13 @@ var parseReducer = exports.parseReducer = function parseReducer(currentType, ful
         }
       });
 
-      return Object.assign({}, state, addToState);
+      (0, _helpers.debugLog)({}, 'state', state, 'addToState', addToState, 'reset', resetFromInitialState);
+
+      return Object.assign({}, state, addToState, resetFromInitialState);
     };
   }
 
-  if ((0, _helpers.isObject)(reducerParams)) {
-    if (reducerParams.reset) {
-      return function () {
-        return Object.assign({}, (0, _helpers.cloneObj)(initialState));
-      };
-    }
-  }
-
-  return reducerParams;
+  return thingsToReduce;
 };
 
 exports['default'] = parseReducer;
--- dist/helpers/index.js
+++ dist/helpers/index.js
@@ -3,6 +3,8 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+var DEBUG_ENABLED = false;
+
 var isArray = exports.isArray = function isArray(obj) {
   return Array.isArray(obj);
 };
@@ -20,4 +22,16 @@ var isObject = exports.isObject = function isObject(obj) {
 
 var cloneObj = exports.cloneObj = function cloneObj(obj) {
   return JSON.parse(JSON.stringify(obj));
+};
+
+var debugLog = exports.debugLog = function debugLog(options) {
+  for (var _len = arguments.length, toLog = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    toLog[_key - 1] = arguments[_key];
+  }
+
+  if (DEBUG_ENABLED) {
+    var _console;
+
+    (_console = console).log.apply(_console, toLog);
+  }
 };
