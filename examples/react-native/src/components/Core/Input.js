b3af718ef152afce111c928716c9447f7b875219
Stefan Koshy <noreply@skoshy.com>
2018-09-17 19:17:29 -0400

    Small input tweaks

--- examples/react-native/src/components/Core/Input.js
+++ examples/react-native/src/components/Core/Input.js
@@ -5,9 +5,12 @@ export const Button = styled.TouchableOpacity`
   padding: 0.25px 1px;
   margin: 0 1px;
   background-color: ${props => props.theme.background};
-  border: 2px solid blue;
+  border: 2px solid ${props => props.theme.contrast};
+  alignItems: center;
+  justifyContent: center;
 `;
 
 export const ButtonText = styled.Text`
+  font-size: 25px;
   color: ${props => props.theme.primary};
 `;
