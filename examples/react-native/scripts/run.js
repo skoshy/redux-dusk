7e4e28542328846babf996a779ddde16d7530d80
Stefan Koshy <noreply@skoshy.com>
2018-09-18 01:26:22 -0400

    Better run script

--- examples/react-native/scripts/run.js
+++ examples/react-native/scripts/run.js
@@ -1,34 +1,30 @@
-try {
-  const os = require('os');
-  const spawn = require('cross-spawn');
-  const argv = require('minimist')(process.argv.slice(2));
+const os = require('os');
+const spawn = require('cross-spawn');
+const argv = require('minimist')(process.argv.slice(2));
 
-  let runPlatform = os.platform() === 'darwin'
-    ? 'ios'
-    : 'android';
+let runPlatform = os.platform() === 'darwin'
+  ? 'ios'
+  : 'android';
 
-  argv._.forEach((cmdParam) => {
-    switch (cmdParam) {
-      case 'android':
-        runPlatform = 'android';
-        break;
-      case 'ios':
-        runPlatform = 'ios';
-        break;
-      default:
-        break;
-    }
-  });
+argv._.forEach((cmdParam) => {
+  switch (cmdParam) {
+    case 'android':
+      runPlatform = 'android';
+      break;
+    case 'ios':
+      runPlatform = 'ios';
+      break;
+    default:
+      break;
+  }
+});
 
-  const child = spawn('react-native', [`run-${runPlatform}`, '--port=50300']);
+const child = spawn(
+  'react-native',
+  [`run-${runPlatform}`, '--port=50300'],
+  { stdio: 'inherit' },
+);
 
-  child.stdout.on('data', (data) => {
-    console.log(data.toString());
-  });
-
-  child.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
-} catch (err) {
-  throw err;
-}
+child.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
