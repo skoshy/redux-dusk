0386e01b73b9058ea491cd93ec39668693d36e9e
Stefan Koshy <noreply@skoshy.com>
2018-09-17 23:19:20 -0400

    Fix so we use the new version of jsc-android JS Core. Android now works!

--- examples/react-native/scripts/run.js
+++ examples/react-native/scripts/run.js
@@ -1,30 +1,34 @@
-const os = require('os');
-const { spawn } = require('child_process');
-const argv = require('minimist')(process.argv.slice(2));
+try {
+  const os = require('os');
+  const spawn = require('cross-spawn');
+  const argv = require('minimist')(process.argv.slice(2));
 
-let runPlatform = os.platform() === 'darwin'
-  ? 'ios'
-  : 'android';
+  let runPlatform = os.platform() === 'darwin'
+    ? 'ios'
+    : 'android';
 
-argv._.forEach((cmdParam) => {
-  switch (cmdParam) {
-    case 'android':
-      runPlatform = 'android';
-      break;
-    case 'ios':
-      runPlatform = 'ios';
-      break;
-    default:
-      break;
-  }
-});
+  argv._.forEach((cmdParam) => {
+    switch (cmdParam) {
+      case 'android':
+        runPlatform = 'android';
+        break;
+      case 'ios':
+        runPlatform = 'ios';
+        break;
+      default:
+        break;
+    }
+  });
 
-const child = spawn('react-native', [`run-${runPlatform}`, '--port=50300']);
+  const child = spawn('react-native', [`run-${runPlatform}`, '--port=50300']);
 
-child.stdout.on('data', (data) => {
-  console.log(data.toString());
-});
+  child.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
 
-child.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+} catch (err) {
+  throw err;
+}
