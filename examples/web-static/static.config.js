bf2fae267116fa45e40bc93a9b20b40b84079148
Stefan Koshy <noreply@skoshy.com>
2018-10-22 15:45:40 -0400

    Remove yml loader, fix building

--- examples/web-static/static.config.js
+++ examples/web-static/static.config.js
@@ -1,42 +1,47 @@
 export default {
   getSiteData: () => ({
-    siteTitle: 'redux-dusk Example',
+    siteTitle: `redux-dusk Example`,
   }),
   getRoutes: async () => {
     return [
       {
-        path: '/',
-        component: 'src/pages/Trello',
+        path: `/`,
+        component: `src/pages/Trello`,
       },
       {
-        path: '/reddit',
-        component: 'src/pages/Reddit',
+        path: `/reddit`,
+        component: `src/pages/Reddit`,
       },
       {
         is404: true,
-        component: 'src/pages/404',
+        component: `src/pages/404`,
       },
     ];
   },
-  webpack: [ // array of functions that can append rules and things to webpack config
-    (config, { defaultLoaders }) => {
-      /* eslint-disable no-param-reassign */
-      config.module.rules = [
-        {
-          oneOf: [
-            defaultLoaders.jsLoader,
-            defaultLoaders.cssLoader,
-            {
-              // YAML Loader
-              loader: 'yml-loader',
-              test: /\.ya?ml$/,
-            },
-            defaultLoaders.fileLoader,
-          ],
-        },
-      ];
-      return config;
-      /* eslint-enable no-param-reassign */
-    },
-  ],
+
+  /*
+  ** there are some issues with customizing webpack for react-static currently.
+  ** see this if you get an error about extract-text-webpack-plugin
+  ** - https://github.com/nozzle/react-static/issues/601#issuecomment-429574588
+  **
+    webpack: [ // array of functions that can append rules and things to webpack config
+      (config, { defaultLoaders }) => {
+        // eslint-disable no-param-reassign
+        config.module.rules = [
+          {
+            oneOf: [
+              defaultLoaders.cssLoader,
+              defaultLoaders.jsLoader,
+              defaultLoaders.fileLoader,
+            ],
+          },
+        ];
+
+        return config;
+        // eslint-enable no-param-reassign
+      },
+    ],
+  **
+  **
+  */
 };
